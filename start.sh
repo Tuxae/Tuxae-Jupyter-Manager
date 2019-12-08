1446ed45f489d950c560e790829beff720f22fe8
zteeed <zteeed@minet.net>
2019-12-08 08:17:14 +0100

    Manage flushing docker components

--- start.sh
+++ start.sh
@@ -11,3 +11,14 @@ orange=`tput setaf 3`
 purple=`tput setaf 5`
 reset=`tput sgr0`
 
+function flush() {
+	# Flush all docker components
+	echo "${red}[!] Flushing all docker components${reset}"
+	echo "${orange}[+] Flushing docker containers${reset}"
+	docker rm --force $(docker ps -a -q)
+	echo "${orange}[+] Flushing docker images${reset}"
+	docker rmi $(docker images -q) --force
+	echo "${orange}[+] Flushing docker volumes${reset}"
+	docker volume rm --force $(docker volume ls -q)  
+}
+
