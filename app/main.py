f66bac001443bf355f4f7739748644b4a298a65f
zteeed <zteeed@minet.net>
2019-12-08 09:19:41 +0000

    Add login endpoint

--- app/main.py
+++ app/main.py
@@ -8,5 +8,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=80, debug=True)
