c13b47f764f2cafd93e79a30b8365d43d2539552
zteeed <zteeed@minet.net>
2019-12-08 17:28:56 +0000

    Manage flask 404 errors

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import werkzeug.exceptions
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,6 +13,11 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
+
+
+@app.errorhandler(404)
+def handle_404(err: werkzeug.exceptions.NotFound):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
