5b5366300e6a189b73da65527dde7aad5da00b9d
zteeed <zteeed@minet.net>
2019-12-08 08:16:40 +0100

    Add term colors

--- start.sh
+++ start.sh
@@ -5,3 +5,9 @@ DIR_PATH=$(dirname $FULL_PATH)
 DEFAULT_EMAIL="aurelien@duboc.xyz"
 DOMAIN="jupyter.t35h.re"
 
+red=`tput setaf 1`
+green=`tput setaf 2`
+orange=`tput setaf 3`
+purple=`tput setaf 5`
+reset=`tput sgr0`
+
