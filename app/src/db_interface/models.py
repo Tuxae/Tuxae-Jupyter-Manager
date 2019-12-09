3ed492415aece5586da65529af578e78fea47a03
zteeed <zteeed@minet.net>
2019-12-09 03:26:30 +0000

    Fix __repr__ instance from WhitelistDomains class

--- app/src/db_interface/models.py
+++ app/src/db_interface/models.py
@@ -30,7 +30,7 @@ class WhitelistDomains(db.Model, UserMixin):
     domain = db.Column(db.String(200), unique=True, nullable=False)
 
     def __repr__(self):
-        return f'<{self.__class__.__name__} {self.name}>'
+        return f'<{self.__class__.__name__} {self.domain}>'
 
 
 class UsersModelView(ModelView):
