35a86d5c9ac072bd4fc1e5c798b5112a83f229dd
zteeed <zteeed@minet.net>
2019-12-10 10:27:33 +0100

    Use docker-compose on bash script start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,6 @@
 
 FULL_PATH=$(realpath $0)
 DIR_PATH=$(dirname $FULL_PATH)
-DEFAULT_EMAIL="aurelien@duboc.xyz"
-DOMAIN="jupyter.t35h.re"
 
 red=`tput setaf 1`
 green=`tput setaf 2`
@@ -22,10 +20,18 @@ function flush() {
 	docker volume rm --force $(docker volume ls -q)  
 }
 
+function build_project() {
+	# Build project using docker-compose 
+	echo "${green}[*] Make all project environment${reset}"
+	echo "${purple}[+] Clean project workspace${reset}"
+	docker-compose down
+	echo "${purple}[+] Build project${reset}"
+	docker-compose build --no-cache
+	echo "${purple}[+] Start project${reset}"
+	docker-compose up -d
+}
+
 function registry() {
-	# Add docker registry listening on localhost
-	echo "${green}[*] Adding local docker registry${reset}"
-	docker run --detach -p 127.0.0.1:5000:5000 --restart=always --name registry registry:2
 	# Add some docker images to local registry
 	echo "${green}[*] Adding jupyter/datascience-notebook to local registry${reset}"
 	echo "${purple}[+] Pulling jupyter/datascience-notebook${reset}"
@@ -36,32 +42,10 @@ function registry() {
 	docker push 127.0.0.1:5000/datascience-notebook
 }
 
-function reverse_proxy() {
-	echo "${green}[*] Manage reverse proxy with letsencrypt SSL certificates${reset}"
-	echo "${purple}[+] Starting jwilder/nginx-proxy${reset}"
-	docker run --detach \
-		--name nginx-proxy \
-		--publish 80:80 \
-		--publish 443:443 \
-		--volume /etc/nginx/certs \
-		--volume /etc/nginx/vhost.d \
-		--volume /usr/share/nginx/html \
-		--volume /var/run/docker.sock:/tmp/docker.sock:ro \
-		jwilder/nginx-proxy
-	echo "${purple}[+] Starting jwilder/nginx-proxy-letsencrypt${reset}"
-	# if necessary use: --env "DEBUG=true" \
-	docker run --detach \
-		--name nginx-proxy-letsencrypt \
-		--volumes-from nginx-proxy \
-		--volume /var/run/docker.sock:/var/run/docker.sock:ro \
-		--env "$DEFAULT_EMAIL" \
-		jrcs/letsencrypt-nginx-proxy-companion
-}
-
 function main() {
 	flush
+	build_project
 	registry
-	reverse_proxy
 }
 
 main
